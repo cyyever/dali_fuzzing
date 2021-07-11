77b9891b4e4a83076770069e778334a6a33473c0
cyy <cyyever@outlook.com>
2021-07-12 01:55:07 +0800

    add code

---
+++ fuzz_test/png_test.cpp
@@ -0,0 +1,20 @@
+/*!
+ * \file bmp_test.cpp
+ *
+ */
+
+#include <dali/image/png.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  try {
+    if (Size <= 2) {
+      return 0;
+    }
+    dali::DALIImageType image_type = static_cast<dali::DALIImageType>(Data[0]);
+    dali::PngImage img(Data + 1, Size - 1, image_type);
+    img.Decode();
+    img.PeekShape();
+  } catch (...) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
